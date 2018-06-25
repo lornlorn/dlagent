2355d7719bf222fc5bac5ae88df5d90b22d00d55
小刁子 <dl_lorn@aliyun.com>
2018-06-25 22:31:46 +0800

    update20180625

--- app/scheduler/crontimer.go
+++ app/scheduler/crontimer.go
@@ -1,11 +1,9 @@
-package monitor
+package scheduler
 
 import (
 	"app/models"
 	"errors"
 	"log"
-	"os/exec"
-	"strings"
 
 	"github.com/robfig/cron"
 )
@@ -77,56 +75,4 @@ func loadJobs(c *cron.Cron) error {
 		}
 	}
 	return nil
-}
-
-func runCmd(cronsh string, croncmd string) ([]byte, error) {
-	var command string
-	var args []string
-
-	if cronsh == "" {
-		cmdargs := strings.Split(croncmd, " ")
-		command = cmdargs[0]
-		args = cmdargs[1:]
-	} else {
-		command = cronsh
-		// cmdargs := "-c " + croncmd
-		// 转换为可变长数组
-		// args = strings.Split(cmdargs, " ")
-		args = []string{"-c", croncmd}
-	}
-	cmd := exec.Command(command, args...)
-	/*
-	   测试start
-	*/
-	// t, _ := exec.LookPath(command)
-	// log.Println(t)
-	// log.Println(filepath.Base(command))
-	/*
-	   测试end
-	*/
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, err
-	}
-
-	if err = cmd.Start(); err != nil {
-		return nil, err
-	}
-
-	var out = make([]byte, 0, 1024)
-	for {
-		tmp := make([]byte, 128)
-		n, err := stdout.Read(tmp)
-		out = append(out, tmp[:n]...)
-		if err != nil {
-			break
-		}
-	}
-
-	if err = cmd.Wait(); err != nil {
-		return nil, err
-	}
-
-	return out, nil
 }
---
+++ app/scheduler/executor.go
@@ -0,0 +1,58 @@
+package scheduler
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func runCmd(cronsh string, croncmd string) ([]byte, error) {
+	var command string
+	var args []string
+
+	if cronsh == "" {
+		cmdargs := strings.Split(croncmd, " ")
+		command = cmdargs[0]
+		args = cmdargs[1:]
+	} else {
+		command = cronsh
+		// cmdargs := "-c " + croncmd
+		// 转换为可变长数组
+		// args = strings.Split(cmdargs, " ")
+		args = []string{"-c", croncmd}
+	}
+	cmd := exec.Command(command, args...)
+	/*
+	   测试start
+	*/
+	// t, _ := exec.LookPath(command)
+	// log.Println(t)
+	// log.Println(filepath.Base(command))
+	/*
+	   测试end
+	*/
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	if err = cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	var out = make([]byte, 0, 1024)
+	for {
+		tmp := make([]byte, 128)
+		n, err := stdout.Read(tmp)
+		out = append(out, tmp[:n]...)
+		if err != nil {
+			break
+		}
+	}
+
+	if err = cmd.Wait(); err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
