608f829ed8e4c49002d2ee0d5d937792f54d26bf
小刁子 <dl_lorn@aliyun.com>
2019-01-08 00:08:17 +0800

    update20190108

--- app/api/html.go
+++ app/api/html.go
@@ -21,21 +21,14 @@ func (html HTML) GetCompWithParams(reqBody []byte, reqURL url.Values) interface{
 	compid, err := strconv.Atoi(reqURL["CompId"][0])
 	if err != nil {
 		seelog.Errorf("strconv.Atoi Error : %v", err)
-	}
-	comp, err := models.GetComponentByID(compid)
-	if err != nil {
-		seelog.Errorf("models.GetComponentByID Error : %v", err)
-	}
-	params, err := models.GetParametersByCompID(compid)
-	if err != nil {
-		seelog.Errorf("models.GetParametersByCompID Error : %v", err)
+		return nil
 	}
 
-	var compWithParams models.ComponentWithParams
-	compWithParams.Comp = comp
-	compWithParams.Params = params
-
-	seelog.Debugf("models.ComponentWithParams : %v", compWithParams)
+	compWithParams, err := models.GetComponentWithParamsByCompID(compid)
+	if err != nil {
+		seelog.Errorf("models.GetComponentWithParamsByCompID Error : %v", err)
+		return nil
+	}
 
 	return compWithParams
 }
--- app/models/userdefine.go
+++ app/models/userdefine.go
@@ -1,1 +1,38 @@
 package models
+
+import (
+	seelog "github.com/cihub/seelog"
+)
+
+/*
+ComponentWithParams struct
+component with parameters
+*/
+type ComponentWithParams struct {
+	Comp   TbComponent
+	Params []TbParameter
+}
+
+/*
+GetComponentWithParamsByCompID func(compid int) (ComponentWithParams, error)
+*/
+func GetComponentWithParamsByCompID(compid int) (ComponentWithParams, error) {
+	comp, err := GetComponentByID(compid)
+	if err != nil {
+		seelog.Errorf("GetComponentByID Error : %v", err)
+		return ComponentWithParams{}, err
+	}
+	params, err := GetParametersByCompID(compid)
+	if err != nil {
+		seelog.Errorf("GetParametersByCompID Error : %v", err)
+		return ComponentWithParams{}, err
+	}
+
+	var compWithParams ComponentWithParams
+	compWithParams.Comp = comp
+	compWithParams.Params = params
+
+	seelog.Debugf("ComponentWithParams : %v", compWithParams)
+
+	return compWithParams, nil
+}
